45bc54985fbeb592d79a170d784d9497578967cc
jasonduquain <jduquain@gmail.com>
2017-10-21 14:35:12 -0400

    first commit - starter files

---
+++ js/main - START.js
@@ -0,0 +1,12 @@
+// Listen for form submit
+
+
+
+
+    /*bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';*/
+
